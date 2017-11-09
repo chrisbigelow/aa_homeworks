332eaaa85447c06b3bc8ed5b41b1470a32cbe57b
Christopher Bigelow <chrisbigelow25@gmail.com>
2017-11-09 06:59:57 -0800

    W6D4

---
+++ W6D4/vanilla-dom/script.js
@@ -0,0 +1,41 @@
+document.addEventListener("DOMContentLoaded", () => {
+  // toggling restaurants
+
+  const toggleLi = (e) => {
+    const li = e.target;
+    if (li.className === "visited") {
+      li.className = "";
+    } else {
+      li.className = "visited";
+    }
+  };
+
+  document.querySelectorAll("#restaurants li").forEach((li) => {
+    li.addEventListener("click", toggleLi);
+  });
+
+
+
+  // adding SF places as list items
+
+  // --- your code here!
+  const placeForm = document.getElementById("list-container form");
+
+  placeForm.addEventListener("submit", event => {
+    
+    event.preventDefault();
+    
+
+
+
+  });
+
+
+
+  // adding new photos
+
+  // --- your code here!
+
+
+
+});
